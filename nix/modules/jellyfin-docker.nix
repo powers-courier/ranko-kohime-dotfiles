50218c98f27adae44fbf31e167183be1e4a80b22
Ranko Kohime <ranko.kohime@runbox.com>
2025-03-27 17:55:26 -0500

    Move multimedia from jellyfin module to discrete module

--- nix/modules/jellyfin-docker.nix
+++ nix/modules/jellyfin-docker.nix
@@ -2,31 +2,8 @@
 {
   imports = [
     ./nfs-share-videos.nix
+    ./packages-multimedia.nix
   ];
-  boot.kernelParams = [
-      "i915.enable_guc=2"
-    ];
-  nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  };
-  hardware = {
-    enableAllFirmware = true;
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = false;
-      extraPackages = with pkgs; [
-        intel-compute-runtime
-        intel-media-driver
-        libva
-        libva-utils
-        libvdpau-va-gl
-        vaapiIntel
-        vaapiVdpau
-        vpl-gpu-rt
-      ];
-    };
-  };
   virtualisation.docker.enable = true;
   virtualisation.docker.daemon.settings.live-restore = false;
   users = {

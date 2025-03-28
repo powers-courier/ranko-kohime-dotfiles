31f256fa64e07e7b68bfbcce693a9e725cc3f477
Ranko Kohime <ranko.kohime@runbox.com>
2025-03-28 07:32:12 -0500

    Docker Compose changes

--- nix/modules/jellyfin-docker.nix
+++ nix/modules/jellyfin-docker.nix
@@ -1,8 +1,5 @@
 { config, pkgs, vars, ... }:
 {
-  imports = [
-    ./packages-multimedia.nix
-  ];
   virtualisation.docker.enable = true;
   virtualisation.docker.daemon.settings.live-restore = false;
 }

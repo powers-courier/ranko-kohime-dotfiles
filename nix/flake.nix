dfe19968b23fc2e8bbdad416a629da62b7b62036
Ranko Kohime <ranko.kohime@runbox.com>
2025-04-09 19:28:41 +0000

    Add rsize, wsize to NFS mount options, modularize options

--- nix/flake.nix
+++ nix/flake.nix
@@ -376,64 +376,64 @@
                 "/Mounts/Disks" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/Disks";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/Documents" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/Documents";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/Downloads" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/Downloads";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/LGC_Actual" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/LGC_Actual";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/LGC_Lore_Table" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/LGC_Lore_Table";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/Library" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/Library";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/Workspace" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/Workspace";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
               };
               fileSystems = {
                 "/Mounts/JellyConfig" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/Videos/JellyConfig";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/Music" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/Music";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/Videos" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/Videos";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/youtube-dl" = {
                   device = "${vars.truenas-ip}:/mnt/youtube-dl/youtube-dl";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/youtube-dl/z.Daily/Finished" = {
                   device = "${vars.truenas-ip}:/mnt/ytdl-fin/Finished";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
               };
         
@@ -660,64 +660,64 @@
                 "/Mounts/Disks" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/Disks";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/Documents" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/Documents";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/Downloads" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/Downloads";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/LGC_Actual" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/LGC_Actual";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/LGC_Lore_Table" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/LGC_Lore_Table";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/Library" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/Library";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/Workspace" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/Workspace";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
               };
               fileSystems = {
                 "/Mounts/JellyConfig" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/Videos/JellyConfig";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/Music" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/Music";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/Videos" = {
                   device = "${vars.truenas-ip}:/mnt/Svartalfheim/Videos";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/youtube-dl" = {
                   device = "${vars.truenas-ip}:/mnt/youtube-dl/youtube-dl";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
                 "/Mounts/youtube-dl/z.Daily/Finished" = {
                   device = "${vars.truenas-ip}:/mnt/ytdl-fin/Finished";
                   fsType = "nfs";
-                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" ];
+                  options = [ "nfsvers=4" "hard" "users" "rw" "exec" "rsize=1048576" "wsize=1048576" ];
                 };
               };
             })

7a77a57a8b862096304a849c72cbd75e2e89d5fc
Ranko Kohime <ranko.kohime@runbox.com>
2025-04-17 20:48:58 +0000

    Add single NVMe disko config

---
+++ nix/disko/single-nvme-zfs-noswap.nix
@@ -0,0 +1,83 @@
+{
+  disko.devices = {
+    disk = {
+      nvme0n1 = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "ESP";
+              start = "1MiB";
+              end = "1GiB";
+              fs-type = "fat32";
+              bootable = true;
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            }
+            {
+              name = "zfs";
+              start = "1GiB";
+              end = "100%";
+              content = {
+                type = "zfs";
+                pool = "zroot";
+              };
+            }
+          ];
+        };
+      };
+    };
+    zpool = {
+      zroot = {
+        type = "zpool";
+        rootFsOptions = {
+          compression = "zstd";
+          ashift = "12";
+          autotrim = "on";
+          checksum = "edonr";
+          "com.sun:auto-snapshot" = "true";
+        };
+        datasets = {
+          "root" = {
+            type = "zfs_fs";
+            mountpoint = "/";
+            options = {
+              mountpoint = "/";
+              canmount = "on";
+            };
+          };
+          "home" = {
+            type = "zfs_fs";
+            mountpoint = "/home";
+            options = {
+              mountpoint = "/home";
+              canmount = "on";
+            };
+          };
+          "nix" = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+            options = {
+              mountpoint = "/nix";
+              canmount = "on";
+            };
+          };
+          "var" = {
+            type = "zfs_fs";
+            mountpoint = "/var";
+            options = {
+              mountpoint = "/var";
+              canmount = "on";
+            };
+          };
+        };
+      };
+    };
+  };
+}

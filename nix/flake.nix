6c1fccffcd8a8a91c5c006fd8be7effe6a80459e
Ranko Kohime <ranko.kohime@runbox.com>
2025-06-10 19:33:06 +0000

    Modularize openssh

--- nix/flake.nix
+++ nix/flake.nix
@@ -221,26 +221,6 @@
       };
     };
     modules = {
-      openssh = { config, lib, ... }: {
-        options.openssh.enable = lib.mkEnableOption "Enable OpenSSH server, and Mosh" // { default = true; };
-        config = lib.mkIf config.openssh.enable {
-          services.openssh = {
-            banner = "Welcome to... wherever you happen to be\n";
-            enable = true;
-            extraConfig ="";
-            openFirewall = true;
-            ports = [ 22 ];
-            settings = {
-              PasswordAuthentication = false;
-              PermitRootLogin = "prohibit-password";
-              PrintMotd = true;
-              StrictModes = true;
-              X11Forwarding = true;
-            };
-          };
-          programs.mosh.enable = true;
-        };
-      };
       systemBlocks = { config, lib, pkgs, system, ... }: {
         config = {
           boot = lib.mkIf (system == "x86_64-linux") {

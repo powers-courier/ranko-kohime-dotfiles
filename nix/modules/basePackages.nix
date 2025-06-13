309e8e42199c5dc70535dc283d2f3ca59fcc7203
Ranko Kohime <ranko.kohime@runbox.com>
2025-06-13 03:35:49 +0000

    Cleanup

--- nix/modules/basePackages.nix
+++ nix/modules/basePackages.nix
@@ -3,13 +3,11 @@
   config = lib.mkIf config.basePackages.enable {
     environment.systemPackages = with pkgs; [
       byobu
-      dar
       git
       glances
       sshfs
       tmux
       tree
-      unison
     ];
   };
 };

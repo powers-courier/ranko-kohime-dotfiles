9f8824cd25bb1896b6490595455fba834d9dc194
Ranko Kohime <ranko.kohime@runbox.com>
2025-11-27 02:36:59 +0000

    Add tailscale module

---
+++ nix/modules/tailscale.nix
@@ -0,0 +1,10 @@
+{ config, lib, ... }: {
+  options.tailscale.enable = lib.mkEnableOption "Enable Tailscale VPN" // { default = true; };
+  config = lib.mkIf config.tailscale.enable {
+    services.tailscale = {
+      enable = true;
+      openFirewall = true;
+      port = 0;
+    };
+  };
+}

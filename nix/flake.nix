2b810195a3770fa3036835a32682cd065372ddc8
Ranko Kohime <ranko.kohime@runbox.com>
2026-03-01 20:56:29 +0000

    Add in Jelly Proxy modules

--- nix/flake.nix
+++ nix/flake.nix
@@ -315,13 +315,83 @@
           specialArgs = { inherit vars; };
           modules = [
             hardwareConfigs.${name}
-            ./modules/cpuLimiter.nix
-            ./modules/defaultBootloader.nix
+            #flakeModules.cpuLimiter
+            flakeModules.defaultBootloader
+            flakeModules.defaultSettings
             ({ pkgs, vars, ... }: {
               networking.hostName = name;
               system.stateVersion = "25.05";
-              
-              
+              networking = {
+                firewall = {
+                  enable = true;
+                  allowedTCPPorts = [ 8096 ];
+                  allowedUDPPorts = [ 5353 ];
+                };
+              };
+              boot.kernel.sysctl = {
+                "net.ipv4.ip_forward" = 1;
+                "net.ipv6.conf.all.forwarding" = 1;
+              };
+              services.networkd-dispatcher = {
+                enable = true;
+                rules."50-tailscale" = {
+                  onState = [ "routable" ];
+                  script = ''
+                    #!/usr/bin/env bash
+                    ${pkgs.ethtool}/bin/ethtool -K enp1s0 rx-udp-gro-forwarding on rx-gro-list off
+                  '';
+                };
+              };
+              services.tailscale = {
+                enable = true;
+                openFirewall = true;
+              };
+              services.nginx = {
+                enable = true;
+                virtualHosts."default" = {
+                  default = true;
+                  listen = [ { addr = "0.0.0.0"; port = 8096; } ];
+                  locations."/" = {
+                    proxyPass = "http://main-host.${vars.tailscale-fqdn}:8096/";
+                    extraConfig = ''
+                      proxy_http_version 1.1;
+                      proxy_set_header Host "main-host.${vars.tailscale-fqdn}";
+                      proxy_set_header X-Real-IP $remote_addr;
+                      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                      proxy_set_header X-Forwarded-Proto $scheme;
+                      proxy_set_header Upgrade $http_upgrade;
+                      proxy_set_header Connection "upgrade";
+                    '';
+                  };
+                };
+              };
+              services.avahi = {
+                enable = true;
+                nssmdns4 = true;
+                publish = {
+                  enable = true;
+                  addresses = true;
+                  userServices = true;
+                };
+              };
+              environment.etc."avahi/services/jellyfin.service".text = ''
+                <?xml version="1.0" standalone='no'?>
+                <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+                <service-group>
+                  <name>Jellyfin Proxy</name>
+                  <service>
+                    <type>_http._tcp</type>
+                    <port>8096</port>
+                    <txt-record>description=Jellyfin Media Server Proxy</txt-record>
+                  </service>
+                </service-group>
+              '';
+              environment.systemPackages = with pkgs; [
+                avahi
+                ethtool
+                nginx
+                tailscale
+              ];
             })
           ];
         };
@@ -330,7 +400,9 @@
     rec {
       nixosModules = flakeModules;
       
-      
+      nixosConfigurations = Jelly-Proxy-Configs // {
+        
+      };
       
     };
 }

aa81a91c7b799f65c63f1bf899008d7064da4676
Ranko Kohime <ranko.kohime@runbox.com>
2026-03-02 00:26:22 +0000

    Add glancesServer module

--- nix/flake.nix
+++ nix/flake.nix
@@ -252,6 +252,22 @@
             time.timeZone = vars.timeZern;
           };
         };
+        glancesServer = { config, lib, pkgs, ... }: {
+          options.glancesServer.enable = lib.mkEnableOption "Enable Glances service" // { default = true; };
+          config = lib.mkIf config.glancesServer.enable {
+            systemd.services.glances-server = {
+              description = "Glances server";
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" ];
+              serviceConfig = {
+                Type = "simple";
+                ExecStart = "${pkgs.glances}/bin/glances -w";
+                RemainAfterExit = false;
+                Restart = "always";
+              };
+            };
+          };
+        };
         fancyKeyboards = { config, lib, pkgs, ... }: {
           options.fancyKeyboards.enable = lib.mkEnableOption "Configuration for fancy keyboards" // { default = false; };
           config = lib.mkIf config.fancyKeyboards.enable {

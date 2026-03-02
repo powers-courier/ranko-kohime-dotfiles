bf847236b7ddac7f886037efdd95ea3515627f9b
Ranko Kohime <ranko.kohime@runbox.com>
2026-03-02 00:25:33 +0000

    Add backupPackages module

--- nix/flake.nix
+++ nix/flake.nix
@@ -198,6 +198,17 @@
         };
       };
       flakeModules = {
+        backupPackages = { config, lib, pkgs, ... }: {
+          options.backupPackages.enable = lib.mkEnableOption "Install backup packages" // { default = true; };
+          config = lib.mkIf config.backupPackages.enable {
+            environment.systemPackages = with pkgs; [
+              dar
+              ddrescue
+              par2cmdline
+              unison
+            ];
+          };
+        };
         basePackages = { config, lib, pkgs, ... }: {
           options.basePackages.enable = lib.mkEnableOption "Install standard packages" // { default = true; };
           config = lib.mkIf config.basePackages.enable {

80d09b7fbd6ae74b52ea9d2aec3e2e5fb94e8906
Ranko Kohime <ranko.kohime@runbox.com>
2026-03-03 02:19:51 +0000

    Add hardware config for jelly-proxy-08

--- nix/flake.nix
+++ nix/flake.nix
@@ -154,6 +154,19 @@
             };
           };
         };
+        jelly-proxy-08 = {
+          fileSystems = {
+            "/" = {
+              device = "/dev/disk/by-uuid/3b3ab7cd-a88b-4023-ba13-b81e90ba7f99";
+              fsType = "ext4";
+            };
+            "/boot" = {
+              device = "/dev/disk/by-uuid/C515-66E7";
+              fsType = "vfat";
+              options = [ "fmask=0077" "dmask=0077" ];
+            };
+          };
+        };
         main-host = {
           fileSystems = {
             "/" =

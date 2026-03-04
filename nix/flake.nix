3eb4d0cc6dd1b1295585de6f4b9b408e00c95a6d
Ranko Kohime <ranko.kohime@runbox.com>
2026-03-04 18:52:59 +0000

    Migrate XFCE Desktop module

--- nix/flake.nix
+++ nix/flake.nix
@@ -214,6 +214,35 @@
         
       };
       flakeModules = {
+        desktopXFCE = { config, lib, pkgs, ... }: {
+          options.desktopXFCE.enable = lib.mkEnableOption "XFCE Desktop" // { default = false; };
+          config = lib.mkIf config.desktopXFCE.enable {
+            environment.systemPackages = with pkgs; [
+              ulauncher
+            ];
+            programs.firefox.enable = true;
+            services = {
+              displayManager = {
+                autoLogin = {
+                  enable = true;
+                  user = "ranko";
+                };
+                defaultSession = "xfce";
+              };
+              xserver = {
+                displayManager = {
+                  lightdm.enable = true;
+                };
+                desktopManager.xfce.enable = true;
+                enable = true;
+                xkb = {
+                  layout = "us";
+                  variant = "";
+                };
+              };
+            };
+          };
+        };
         jellyfinProxyHost = { config, lib, pkgs, ... }: {
           options.jellyfinProxyHost.enable = lib.mkEnableOption "Enable proxy hosting for Jellyfin server" // { default = false; };
           config = lib.mkIf config.jellyfinProxyHost.enable {

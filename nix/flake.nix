a17fe391a5730a7194f606c787d26d52ba58aa69
Ranko Kohime <ranko.kohime@runbox.com>
2026-03-14 07:26:56 +0000

    Migrate opensshServer module to discrete file

--- nix/flake.nix
+++ nix/flake.nix
@@ -542,26 +542,6 @@
             };
           };
         };
-        opensshServer = { config, lib, ... }: {
-          options.openssh.enable = lib.mkEnableOption "Enable OpenSSH server, and Mosh" // { default = true; };
-          config = lib.mkIf config.openssh.enable {
-            services.openssh = {
-              banner = "Welcome to... wherever you happen to be\n";
-              enable = true;
-              extraConfig ="";
-              openFirewall = true;
-              ports = [ 22 ];
-              settings = {
-                PasswordAuthentication = false;
-                PermitRootLogin = "prohibit-password";
-                PrintMotd = true;
-                StrictModes = true;
-                X11Forwarding = true;
-              };
-            };
-            programs.mosh.enable = true;
-          };
-        };
         tailscaleVPN = { config, lib, ... }: {
           options.tailscale.enable = lib.mkEnableOption "Enable Tailscale VPN" // { default = true; };
           config = lib.mkIf config.tailscale.enable {

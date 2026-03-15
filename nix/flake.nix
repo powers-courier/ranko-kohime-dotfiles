1050d2ceb8f30b927c08ce55129def365b60166c
Ranko Kohime <ranko.kohime@runbox.com>
2026-03-15 06:24:01 +0000

    Migrate glancesServer module

--- nix/flake.nix
+++ nix/flake.nix
@@ -464,22 +464,6 @@
             };
           };
         };
-        glancesServer = { config, lib, pkgs, ... }: {
-          options.glancesServer.enable = lib.mkEnableOption "Enable Glances service" // { default = true; };
-          config = lib.mkIf config.glancesServer.enable {
-            systemd.services.glances-server = {
-              description = "Glances server";
-              wantedBy = [ "multi-user.target" ];
-              after = [ "network.target" ];
-              serviceConfig = {
-                Type = "simple";
-                ExecStart = "${pkgs.glances}/bin/glances -w";
-                RemainAfterExit = false;
-                Restart = "always";
-              };
-            };
-          };
-        };
           };
         };
         lokale = { config, lib, ... }: {

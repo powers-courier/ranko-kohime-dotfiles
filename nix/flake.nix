ae4eb1df668bb13f57d8cab43073f5a2ab2cc7c7
Ranko Kohime <ranko.kohime@runbox.com>
2026-03-28 01:54:53 +0000

    Re-integrate zramSwap module

--- nix/flake.nix
+++ nix/flake.nix
@@ -554,6 +554,14 @@
               syncoid = {
                 enable = true;
               };
+        zramSwap = { config, lib, ... }: {
+          options.dangerSwap.enable = lib.mkEnableOption "Enable zram compressed swap" // { default = true; };
+          config = lib.mkIf config.dangerSwap.enable {
+            zramSwap = {
+              enable = true;
+              priority = 1;
+              memoryPercent = 100;
+              swapDevices = 1;
             };
           };
         };

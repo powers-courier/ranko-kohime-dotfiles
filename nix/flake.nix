744767d75269f6e7dc9f434b066ab1373c167ce3
Ranko Kohime <ranko.kohime@runbox.com>
2026-03-29 23:29:46 +0000

    Re-merge defaultNanoRC

--- nix/flake.nix
+++ nix/flake.nix
@@ -293,6 +293,24 @@
             };
           };
         };
+        defaultNanoRC = { config, lib, programs, ... }: {
+          options.defaultNanoRC.enable = lib.mkEnableOption "Sane defaults for nanorc" // { default = true; };
+          config = lib.mkIf config.defaultNanoRC.enable {
+            programs.nano = lib.mkForce {
+              enable = true;
+              nanorc = ''
+                set autoindent
+                set boldtext
+                set constantshow
+                set linenumbers
+                set nowrap
+                set smarthome
+                set tabsize 2
+                set tabstospaces
+              '';
+            };
+          };
+        };
         jellyfinProxyHost = { config, lib, pkgs, ... }: {
           options.jellyfinProxyHost.enable = lib.mkEnableOption "Enable proxy hosting for Jellyfin server" // { default = false; };
           config = lib.mkIf config.jellyfinProxyHost.enable {

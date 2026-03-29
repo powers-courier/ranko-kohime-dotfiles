ada596bcfa3c26d8f7fbf6cc6ba2ec6efd0697e6
Ranko Kohime <ranko.kohime@runbox.com>
2026-03-29 23:41:04 +0000

    Re-merge fancyKeyboards

--- nix/flake.nix
+++ nix/flake.nix
@@ -373,6 +373,15 @@
             '';
           };
         };
+        fancyKeyboards = { config, lib, pkgs, ... }: {
+          options.fancyKeyboards.enable = lib.mkEnableOption "Configuration for fancy keyboards" // { default = false; };
+          config = lib.mkIf config.fancyKeyboards.enable {
+            environment.systemPackages = with pkgs; [
+              system76-keyboard-configurator
+            ];
+            hardware.keyboard.zsa.enable = true;
+          };
+        };
         cpuAmdOptimizations = { config, lib, pkgs, ... }: {
           options.cpuAmdOptimizations.enable = lib.mkEnableOption "Optimizations for AMD Hardware" // { default = false; };
           config = lib.mkIf config.cpuAmdOptimizations.enable {

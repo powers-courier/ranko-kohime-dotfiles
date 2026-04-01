0c545051849718dc784206d7d32b163d848db7b2
Ranko Kohime <ranko.kohime@runbox.com>
2026-04-01 03:29:02 +0000

    Re-export zramSwap module

--- nix/flake.nix
+++ nix/flake.nix
@@ -1070,17 +1070,6 @@
             };
           };
         };
-        zramSwap = { config, lib, ... }: {
-          options.dangerSwap.enable = lib.mkEnableOption "Enable zram compressed swap" // { default = true; };
-          config = lib.mkIf config.dangerSwap.enable {
-            zramSwap = {
-              enable = true;
-              priority = 1;
-              memoryPercent = 100;
-              swapDevices = 1;
-            };
-          };
-        };
       };
       
       jellyProxyHosts = builtins.filter
---
+++ nix/modules/zramSwap.nix
@@ -0,0 +1,11 @@
+{ config, lib, ... }: {
+  options.dangerSwap.enable = lib.mkEnableOption "Enable zram compressed swap" // { default = true; };
+  config = lib.mkIf config.dangerSwap.enable {
+    zramSwap = {
+      enable = true;
+      priority = 1;
+      memoryPercent = 100;
+      swapDevices = 1;
+    };
+  };
+}

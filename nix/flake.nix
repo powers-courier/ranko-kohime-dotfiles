3c271fbd6d432ddc8258388d9dfe0e8e1845d413
Ranko Kohime <ranko.kohime@runbox.com>
2026-04-02 17:54:26 +0000

    Re-migrate lokale

--- nix/flake.nix
+++ nix/flake.nix
@@ -578,25 +578,6 @@
             
           };
         };
-        lokale = { config, lib, ... }: {
-          options.lokale.enable = lib.mkEnableOption "Set Locale" // { default = true; };
-          config = lib.mkIf config.lokale.enable {
-            i18n = {
-              defaultLocale = vars.loKale;
-              extraLocaleSettings = {
-                LC_ADDRESS = vars.loKale;
-                LC_IDENTIFICATION = vars.loKale;
-                LC_MEASUREMENT = vars.loKale;
-                LC_MONETARY = vars.loKale;
-                LC_NAME = vars.loKale;
-                LC_NUMERIC = vars.loKale;
-                LC_PAPER = vars.loKale;
-                LC_TELEPHONE = vars.loKale;
-                LC_TIME = vars.loKale;
-              };
-            };
-          };
-        };
         defaultNanoRC = { config, lib, programs, ... }: {
           options.defaultNanoRC.enable = lib.mkEnableOption "Sane defaults for nanorc" // { default = true; };
           config = lib.mkIf config.defaultNanoRC.enable {
---
+++ nix/modules/tailscaleVPN.nix
@@ -0,0 +1,10 @@
+{ config, lib, ... }: {
+  options.tailscaleVPN.enable = lib.mkEnableOption "Enable Tailscale VPN" // { default = true; };
+  config = lib.mkIf config.tailscaleVPN.enable {
+    services.tailscale = {
+      enable = true;
+      openFirewall = true;
+      port = 0;
+    };
+  };
+}

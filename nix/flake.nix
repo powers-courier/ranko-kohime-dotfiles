4c319daec3a1d351a6ba255eea9bbb9799569ff2
Ranko Kohime <ranko.kohime@runbox.com>
2026-04-02 23:05:22 +0000

    Re-migrate defaultBootloader

--- nix/flake.nix
+++ nix/flake.nix
@@ -141,18 +141,6 @@
             ];
           };
         };
-        defaultBootloader = { config, lib, ... }: {
-          options.defaultBootloader.enable = lib.mkEnableOption "Set default bootloader" // { default = true; };
-          config = lib.mkIf config.defaultBootloader.enable {
-            boot = {
-              initrd.availableKernelModules = [ "ahci" "nvme" "sd_mod" "thunderbolt" "usb_storage" "xhci_pci" ];
-              loader = {
-                efi.canTouchEfiVariables = true;
-                systemd-boot.enable = true;
-              };
-            };
-          };
-        };
         cpuLimiterIntel = { config, lib, ... }: {
           options.cpuLimiterIntel.enable = lib.mkEnableOption "Set CPU Limiter on boot for Intel" // { default = false; };
           config = lib.mkIf config.cpuLimiterIntel.enable {
---
+++ nix/modules/defaultBootloader.nix
@@ -0,0 +1,12 @@
+{ config, lib, ... }: {
+  options.defaultBootloader.enable = lib.mkEnableOption "Set default bootloader" // { default = true; };
+  config = lib.mkIf config.defaultBootloader.enable {
+    boot = {
+      initrd.availableKernelModules = [ "ahci" "nvme" "sd_mod" "thunderbolt" "usb_storage" "xhci_pci" ];
+      loader = {
+        efi.canTouchEfiVariables = true;
+        systemd-boot.enable = true;
+      };
+    };
+  };
+}

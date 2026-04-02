1dbc01bd0686cd19d5cb2877a8b49f542387f24b
Ranko Kohime <ranko.kohime@runbox.com>
2026-04-02 23:18:24 +0000

    Re-migrate nixGarbageCollection

--- nix/flake.nix
+++ nix/flake.nix
@@ -406,34 +406,6 @@
             };
           };
         };
-        nixGarbageCollection = { config, lib, ... }:
-        with lib;
-        {
-          options = {
-            services.nixGarbageCollection = {
-              enable = mkEnableOption "automatic Nix garbage collection" // {
-                default = true;
-              };
-              dates = mkOption {
-                type = types.str;
-                default = "monthly";
-                description = "How often to run garbage collection";
-              };
-              options = mkOption {
-                type = types.str;
-                default = "--delete-older-than 365d";
-                description = "Options to pass to nix-collect-garbage";
-              };
-            };
-          };
-          config = mkIf config.services.nixGarbageCollection.enable {
-            nix.gc = {
-              automatic = true;
-              dates = config.services.nixGarbageCollection.dates;
-              options = config.services.nixGarbageCollection.options;
-            };
-          };
-        };
         glancesServer = { config, lib, pkgs, ... }: {
           options.glancesServer.enable = lib.mkEnableOption "Enable Glances service" // { default = true; };
           config = lib.mkIf config.glancesServer.enable {

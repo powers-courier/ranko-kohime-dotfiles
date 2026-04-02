242528834e0dc7e8b3136624a35da671c425be6b
Ranko Kohime <ranko.kohime@runbox.com>
2026-04-02 17:58:19 +0000

    Re-migrate defaultNanoRC

---
+++ nix/modules/defaultNanoRC.nix
@@ -0,0 +1,18 @@
+{ config, lib, programs, ... }: {
+  options.defaultNanoRC.enable = lib.mkEnableOption "Sane defaults for nanorc" // { default = true; };
+  config = lib.mkIf config.defaultNanoRC.enable {
+    programs.nano = lib.mkForce {
+      enable = true;
+      nanorc = ''
+        set autoindent
+        set boldtext
+        set constantshow
+        set linenumbers
+        set nowrap
+        set smarthome
+        set tabsize 2
+        set tabstospaces
+      '';
+    };
+  };
+}

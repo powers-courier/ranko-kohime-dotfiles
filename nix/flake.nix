688fbed81e652afba50f8b0c334ecd6d6236b784
Ranko Kohime <ranko.kohime@runbox.com>
2026-04-02 23:17:37 +0000

    Re-migrate defaultNetworking

--- nix/flake.nix
+++ nix/flake.nix
@@ -141,35 +141,6 @@
             ];
           };
         };
-        defaultNetworking = { config, lib, ... }: {
-          options.defaultNetworking = {
-            enable = lib.mkEnableOption "Default network settings" // { default = true; };
-            useIwd = lib.mkEnableOption "Enable IWD Wireless Client" // { default = true; };
-          };
-          config = lib.mkIf config.defaultNetworking.enable {
-            networking = {
-              networkmanager = {
-                enable = true;
-                wifi.backend = "iwd";
-              };
-              wireless.iwd = lib.mkIf config.defaultNetworking.useIwd {
-                enable = lib.mkDefault true;
-                settings = {
-                  General = {
-                    EnableNetworkConfiguration = true;
-                  };
-                  Network = {
-                    EnableIPv6 = true;
-                    RoutePriorityOffset = 300;
-                  };
-                  Settings = {
-                    AutoConnect = true;
-                  };
-                };
-              };
-            };
-          };
-        };
         defaultSettings = { config, lib, ... }: {
           options.defaultSettings.enable = lib.mkEnableOption "Default system settings" // { default = true; };
           config = lib.mkIf config.defaultSettings.enable {

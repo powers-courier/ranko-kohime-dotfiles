6305278f552c3973518f2e70e3ceebb5760fd879
Ranko Kohime <ranko.kohime@runbox.com>
2026-04-03 00:15:22 +0000

    Re-migrate backupPackages

--- nix/flake.nix
+++ nix/flake.nix
@@ -130,17 +130,6 @@
         
       };
       flakeModules = {
-        backupPackages = { config, lib, pkgs, ... }: {
-          options.backupPackages.enable = lib.mkEnableOption "Install backup packages" // { default = true; };
-          config = lib.mkIf config.backupPackages.enable {
-            environment.systemPackages = with pkgs; [
-              dar
-              ddrescue
-              par2cmdline
-              unison
-            ];
-          };
-        };
         desktopXFCE = { config, lib, pkgs, ... }: {
           options.desktopXFCE.enable = lib.mkEnableOption "XFCE Desktop" // { default = false; };
           config = lib.mkIf config.desktopXFCE.enable {
---
+++ nix/modules/backupPackages.nix
@@ -0,0 +1,13 @@
+# These packages are for file/disk backups, and disk archiving
+# https://github.com/animetosho/par2cmdline-turbo
+{ config, lib, pkgs, ... }: {
+  options.backupPackages.enable = lib.mkEnableOption "Install backup packages" // { default = true; };
+  config = lib.mkIf config.backupPackages.enable {
+    environment.systemPackages = with pkgs; [
+      dar
+      ddrescue
+      par2cmdline
+      unison
+    ];
+  };
+}

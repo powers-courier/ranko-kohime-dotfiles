1d31d4b9b5d797bbdf2f79c83d3fbb0e9eeb190a
Ranko Kohime <ranko.kohime@runbox.com>
2026-04-05 00:57:04 +0000

    Add multimediaPackages module

---
+++ nix/modules/multimediaPackages.nix
@@ -0,0 +1,22 @@
+# python3.12-musicbrainzngs is a dependency of Ex Falso
+# Quod Libet brings in Ex Falso
+
+{ config, lib, pkgs, ... }: {
+  options.multimediaPackages.enable = lib.mkEnableOption "Install multimedia packages"
+  // { default = false; };
+  config = lib.mkIf config.multimediaPackages.enable {
+    environment.systemPackages = with pkgs; [
+      flac
+      gimp3-with-plugins
+      handbrake
+      mediainfo
+      mkvtoolnix
+      mpv
+      mpvc
+      python312Packages.musicbrainzngs
+      quodlibet
+      vorbisgain
+      vorbis-tools
+    ];
+  };
+}

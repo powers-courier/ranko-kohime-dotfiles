fb78d25e04ada18e5c8be2c3ef628f526ca837d2
Ranko Kohime <ranko.kohime@runbox.com>
2026-04-08 03:56:06 +0000

    Re-migrate desktopXFCE

--- nix/flake.nix
+++ nix/flake.nix
@@ -136,36 +136,6 @@
         
       };
       flakeModules = {
-        desktopXFCE = { config, lib, pkgs, ... }: {
-          options.desktopXFCE.enable = lib.mkEnableOption "XFCE Desktop" // { default = false; };
-          config = lib.mkIf config.desktopXFCE.enable {
-            environment.systemPackages = with pkgs; [
-              thunar-volman
-              ulauncher
-            ];
-            programs.firefox.enable = true;
-            services = {
-              displayManager = {
-                autoLogin = {
-                  enable = true;
-                  user = "ranko";
-                };
-                defaultSession = "xfce";
-              };
-              xserver = {
-                displayManager = {
-                  lightdm.enable = true;
-                };
-                desktopManager.xfce.enable = true;
-                enable = true;
-                xkb = {
-                  layout = "us";
-                  variant = "";
-                };
-              };
-            };
-          };
-        };
         flakeyRouter = { config, lib, pkgs, ... }:
         
         let

0064278c8bfa74dce5cb1a742c0b7a55ea6b4d12
Ranko Kohime <ranko.kohime@runbox.com>
2026-04-15 18:57:35 +0000

    Update n100-1 host

--- nix/flake.nix
+++ nix/flake.nix
@@ -117,22 +117,8 @@
           };
           system.stateVersion = "25.05";
         };
-        n100 = {
-          fileSystems = {
-            "/" = {
-              device = "/dev/disk/by-uuid/3baf4636-0456-44fc-a48b-72fbb49cea3f";
-              fsType = "ext4";
-            };
-            "/boot" = {
-              device = "/dev/disk/by-uuid/9BBB-EE1C";
-              fsType = "vfat";
-              options = [ "fmask=0077" "dmask=0077" ];
-            };
-          };
-          system.stateVersion = "25.05";
-        };
       };
-      };
+      
       flakeModules = {
         flakeyRouter = { config, lib, pkgs, ... }:
         
@@ -599,6 +585,27 @@
                     }];
                     mtu = 9000;
                   };
+                };
+              };
+              system.stateVersion = "25.05";
+            }
+          ];
+        };
+        n100-1 = mkHost {
+          hostname = "n100-1";
+          system = "x86_64-linux";
+          cpuVendor = "intel";
+          extraModules = [
+            {
+              fileSystems = {
+                "/" = {
+                  device = "/dev/disk/by-uuid/3baf4636-0456-44fc-a48b-72fbb49cea3f";
+                  fsType = "ext4";
+                };
+                "/boot" = {
+                  device = "/dev/disk/by-uuid/9BBB-EE1C";
+                  fsType = "vfat";
+                  options = [ "fmask=0077" "dmask=0077" ];
                 };
               };
               system.stateVersion = "25.05";

56cb49ee56b1892cedce1a2148cd707bedf56f24
Ranko Kohime <ranko.kohime@runbox.com>
2026-05-02 03:49:30 +0000

    Migrate host framework-13 to discrete file

--- nix/flake.nix
+++ nix/flake.nix
@@ -498,50 +498,6 @@
       nixosModules = autoModules // flakeModules ;
       homeConfigurations = nixpkgs.lib.genAttrs home-manager-usernames mkHome;
       nixosConfigurations = Jelly-Proxy-Configs // {
-        framework-13 = mkHost {
-          hostname = "framework-13";
-          system = "x86_64-linux";
-          cpuVendor = "intel";
-          role = "laptop";
-          extraModules = [
-            { desktopXFCE.enable = true; }
-            { zfsBootOptions.enable = true; }
-            {
-              fileSystems = {
-                "/" = {
-                  device = "zroot/root";
-                  fsType = "zfs";
-                  options = [ "zfsutil" ];
-                };
-                "/boot" = {
-                  device = "/dev/disk/by-uuid/F83E-8932";
-                  fsType = "vfat";
-                  options = [ "fmask=0077" "dmask=0077" ];
-                };
-                "/home" = {
-                  device = "zroot/home";
-                  fsType = "zfs";
-                  options = [ "zfsutil" ];
-                };
-                "/nix" = {
-                  device = "zroot/nix";
-                  fsType = "zfs";
-                  options = [ "zfsutil" ];
-                };
-                "/var" = {
-                  device = "zroot/var";
-                  fsType = "zfs";
-                  options = [ "zfsutil" ];
-                };
-              };
-              networking = {
-                hostId = "af2f1b7f";
-              };
-              swapDevices = [ ];
-              system.stateVersion = "25.05";
-            }
-          ];
-        };
         router-ask = mkHost {
           hostname = "router-ask";
           system = "x86_64-linux";

545d035e4a2453c4e2563f97e224428ccc374cde
Ranko Kohime <ranko.kohime@runbox.com>
2026-05-09 05:29:53 +0000

    Migrate autoModules to discrete file (not working)

--- nix/flake.nix
+++ nix/flake.nix
@@ -336,18 +336,6 @@
           };
         };
       };
-      autoModules = let
-        modulesDir = ./modules;
-        moduleEntries = builtins.readDir modulesDir;
-        nixModuleFiles = builtins.filter
-          (name: builtins.match ".*\\.nix" name != null)
-          (builtins.attrNames moduleEntries);
-      in builtins.listToAttrs (builtins.map
-        (name: {
-          name = builtins.replaceStrings [".nix"] [""] name;
-          value = import (modulesDir + "/${name}");
-        })
-        nixModuleFiles);
       # ------
       autoDarwinHosts = let
         darwinHostsDir = ./hosts/darwin;

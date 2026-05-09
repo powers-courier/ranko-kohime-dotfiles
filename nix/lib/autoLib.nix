beb461aa894b986778962587d3b6a70a8afc1f61
Ranko Kohime <ranko.kohime@runbox.com>
2026-05-09 05:28:45 +0000

    Add new autoLib function to load functions from discrete files

---
+++ nix/lib/autoLib.nix
@@ -0,0 +1,27 @@
+{ inputs, lib, ... }@args:
+
+let
+  libDir = ./.;
+
+  entries = builtins.readDir libDir;
+
+  nixFiles = builtins.filter
+    (name:
+      builtins.match ".*\\.nix" name != null
+      && name != "autoLib.nix"
+    )
+    (builtins.attrNames entries);
+
+  importedLibs = builtins.listToAttrs (builtins.map
+    (name:
+      let
+        baseName = builtins.replaceStrings [".nix"] [""] name;
+        imported = import (libDir + "/${name}") args;
+      in
+      {
+        name = baseName;
+        value = imported;
+      })
+    nixFiles);
+in
+  importedLibs

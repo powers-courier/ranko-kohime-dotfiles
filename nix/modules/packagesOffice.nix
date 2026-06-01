e26141f0eec3634ce10dbf8155aa3b68055be2b3
Ranko Kohime <ranko.kohime@runbox.com>
2026-06-01 04:05:43 +0000

    Add module packagesOffice

---
+++ nix/modules/packagesOffice.nix
@@ -0,0 +1,11 @@
+{ config, lib, pkgs, ... }: {
+  options.packagesOffice.enable = lib.mkEnableOption "Install Office packages" //
+    { default = false; };
+  config = lib.mkIf config.packagesOffice.enable {
+    environment.systemPackages = with pkgs; [
+      evince
+      gnucash
+      libreoffice-fresh
+    ];
+  };
+}
